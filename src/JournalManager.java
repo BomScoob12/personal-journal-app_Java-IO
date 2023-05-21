2eed06bc33e2646a9077e8a680834959ee93e6ab
BomScoob12 <sorawitkraukham@gmail.com>
2023-05-21 18:16:22 +0700

    Update JournalManager.java

--- src/JournalManager.java
+++ src/JournalManager.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 
 public class JournalManager {
     public void writeEntry(JournalEntry entry){
